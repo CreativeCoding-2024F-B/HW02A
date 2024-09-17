23d62c0e53a9f63d07793ed4ce2d2eb2407b31ed
becdaniels <rebeccadaniels9@gmail.com>
2024-09-17 12:40:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,43 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(200, 120, 220); 
-  rect(100, 100, 200, 100, 10);
-  ellipse(200, 300, 40);
+  background(200, 120, 220);
 
-  let rectX = 300;
-  let rectY = 300; 
-  let rectW = 200;
-
-  fill(200, 17, 3);
-  rect(rectX, rectY, rectW);
-  ellipse(rectX + rectW/2, rectY + rectW/2, 100);
-
-  fill(250, 200, 3);
-  stroke(10, 100, 20);
+  
+  push();
+  stroke(150, 75, 0);
   strokeWeight(10);
-  ellipse(width /2, height /2, 100);
+  ellipse(720, 260, 80);
+  pop();
+
+  fill(0, 0, 255);
+  quad(130, 240, 250, 160, 480, 250, 300, 450);
+
+  push();
+  translate(300, 100); 
+  rotate(45);
+  fill(2);
+  rect(-20, 100, 50, 200);
+  pop();
+
+  fill(2); 
+  rect(500, 400, 200);
+  fill(150, 75, 0);
+  rect(760, 120, 90, 80);
+  fill(250, 200, 3);
+  rect(800, 200, 100, 200);
+
+  
+  //let rectX = 300;
+  //let rectY = 300; 
+  //let rectW = 200;
+
+  //fill(200, 17, 3);
+  //rect(rectX, rectY, rectW);
+  //ellipse(rectX + rectW/2, rectY + rectW/2, 100);
+
+  
+
+  
 }
 
 function draw() {
