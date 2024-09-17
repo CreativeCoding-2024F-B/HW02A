6eeaf645d21912fd54b24390501100aeacf415ac
becdaniels <rebeccadaniels9@gmail.com>
2024-09-16 20:40:05 -0400

    drew shapes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(200, 120, 220); 
-  rect(100, 100, 200);
+  rect(100, 100, 200, 100, 10);
+  ellipse(200, 300, 40);
+
+  let rectX = 300;
+  let rectY = 300; 
+  let rectW = 200;
+
+  rect(rectX, rectY, rectW);
+  ellipse(rectX + rectW/2, rectY + rectW/2, 100);
 }
 
 function draw() {
